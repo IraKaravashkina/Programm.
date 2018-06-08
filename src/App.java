2f56bffde8199388e7ea2c88fcbea886b16524b0
IraKaravashkina <37302627+IraKaravashkina@users.noreply.github.com>
2018-06-08 17:22:15 +0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,19 +1,16 @@
-public class App {
-}
-        import dao.ContactDao;
-        import dao.impl.DBContactDao;
-        import services.ContactService;
-        import services.impl.FSContactServiceImpl;
-        import view.CmdLineService;
-        import view.impl.CmdLineServiceImpl;
-        import java.io.IOException;
+import dao.ContactDao;
+import dao.impl.DBContactDao;
+import services.ContactService;
+import services.impl.FSContactServiceImpl;
+import view.CmdLineService;
+import view.impl.CmdLineServiceImpl;
+
+import java.io.IOException;
 
 public class App {
 
     /**
-
      * Начало программы. Тут запускается программа, создаются все сервиса и устанавливаются связи между ними.
-
      */
 
     public static void main(String[] args) throws IOException {
