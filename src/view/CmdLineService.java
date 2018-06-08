3aa974b90f10ad69d668858299537f0045196363
IraKaravashkina <37302627+IraKaravashkina@users.noreply.github.com>
2018-06-08 17:36:14 +0300

    Update CmdLineService.java

--- src/view/CmdLineService.java
+++ src/view/CmdLineService.java
@@ -1,9 +1,8 @@
 package view;
 
-public class CmdLineService {
-}
-          import java.io.IOException;
+import java.io.IOException;
 
 public interface CmdLineService {
+
     void runMenu() throws IOException;
 }
