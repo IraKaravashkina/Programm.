7e7b28fb2ef7170f407a9c508ec77567852a13b7
IraKaravashkina <37302627+IraKaravashkina@users.noreply.github.com>
2018-06-08 17:46:26 +0300

    Update ContactServicelmpl.java

--- src/services/impl/ContactServicelmpl.java
+++ src/services/impl/ContactServicelmpl.java
@@ -1,46 +1,43 @@
 package services.impl;
 
-public class ContactServicelmpl {
-}
-        import programm.model.Contact;
-        import programm.services.ContactService;
-        import java.util.HashMap;
-        import java.util.Map;
-        import javafx.collections.FXCollections;
-        import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.Contact;
+import services.ContactService;
 
-public class ContactServiceImpl implements ContactService {
 
-    /**
+    public class ContactServiceImpl implements ContactService {
 
-     * Реализация ContactService которая использует Map для хранения данных.
+        /**
+         * Реализация ContactService которая использует Map для хранения данных.
+         */
 
-     */
+        private final ObservableList<Contact> contactList;
 
-    private final ObservableList<Contact> contactList;
-    public ContactServiceImpl() {
-        this.contactList = FXCollections.observableArrayList();
-    }
+        public ContactServiceImpl() {
+            this.contactList = FXCollections.observableArrayList();
+        }
 
-    @Override
-    public void createContact(String name, int phone) {
-        contactList.add(new Contact(name, phone));
-    }
+        @Override
+        public void createContact(String name, int phone) {
+            contactList.add(new Contact(name, phone));
+        }
 
-    @Override
-    public void deleteContact(String name) {
+        @Override
+        public void deleteContact(String name) {
 //        contactList.remove(name);
-    }
+        }
 
-    @Override
-    public ObservableList<Contact> showContacts() {
-        return contactList;
-    }
+        @Override
+        public ObservableList<Contact> showContacts() {
+            return contactList;
+        }
 
-    @Override
-    public void editContact(String name, String newName, int newAge) {
+        @Override
+        public void editContact(String name, String newName, int newAge) {
 //        Contact contact = contactList.get(name);
 //        contact.setName(newName);
 //        contact.setAge(newAge);
+        }
     }
-}
+
